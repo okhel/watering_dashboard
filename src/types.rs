c87a514535199c9254c7381c7b0eb2c6e0e8fe3b
Ole Helgedagsrud <olekaasa@gmail.com>
2026-05-15 15:25:55 +0200

    refactor capacitive measurement name

--- src/types.rs
+++ src/types.rs
@@ -2,8 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Message {
-    /// Moisture in centi-percent: 4250 = 42.50 %.
-    Moisture { value_cpct: u16 },
+    Moisture { adc_raw: u16 },
     /// Pump fired for this long.
     Pump { duration_ms: u32 },
 }
